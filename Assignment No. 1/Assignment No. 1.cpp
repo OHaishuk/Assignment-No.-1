a9f3256e49b7497c506720a2b6ff7f3b1bf8d9ec
OHaishuk <haishol2296_aehit@students.vizja.pl>
2025-10-17 19:43:04 +0200

    Add project files.

---
+++ Assignment No. 1/Assignment No. 1.cpp
@@ -0,0 +1,28 @@
+#include <array>
+#include <iostream>
+const int MAXNUMBEROFROWS = 3; // Any value can be inserted here
+const int MAXNUMBEROFCOLUMNS = 3; // Any value can be inserted here as well
+void printArray(std::array < std::array<int, MAXNUMBEROFCOLUMNS>, MAXNUMBEROFROWS>& arr, int nrrows, int nrcolumns) {
+	for (int i = 0; i < nrrows; ++i) {
+		for (int j = 0; j < nrcolumns; ++j) {
+			std::cout << arr[i][j] << " ";
+		}
+		std::cout << std::endl;
+	}
+}
+void fillArray(std::array < std::array<int, MAXNUMBEROFCOLUMNS>, MAXNUMBEROFROWS>& arr, int nrrows, int nrcolumns) {
+	for (int i = 0; i < nrrows; ++i) {
+		for (int j = 0; j < nrcolumns; ++j) {
+			std::cout << "row " << i + 1 << " column " << j + 1 << " : ";
+			std::cin >> arr[i][j];
+		}
+	}
+
+}
+
+int main() {
+	std::array<std::array<int, MAXNUMBEROFCOLUMNS>, MAXNUMBEROFROWS > matrix;
+	fillArray(matrix, MAXNUMBEROFCOLUMNS, MAXNUMBEROFROWS);
+	printArray(matrix, MAXNUMBEROFCOLUMNS, MAXNUMBEROFROWS);
+	return 0;
+}
